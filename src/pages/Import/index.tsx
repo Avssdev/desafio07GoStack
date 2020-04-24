781b83a4ec3308455ff0b34b83842e1c296dfcc2
Alvaro S <avssdev.development@gmail.com>
2020-04-23 23:02:24 -0300

    Finalizada parte de upload de arquivo

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,33 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.map(csv => data.append('file', csv.file, csv.name));
+
+    console.log(data.getAll('file'));
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
+
+    history.push('/');
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const fileWithProps: FileProps[] = files.map(file => {
+      const fileProps: FileProps = {
+        file,
+        name: file.name,
+        readableSize: filesize(file.size),
+      };
+
+      return fileProps;
+    });
+
+    setUploadedFiles([...uploadedFiles, ...fileWithProps]);
   }
 
   return (
